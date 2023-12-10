493a909e035f10714bef442e381b177ee96d7a67
natasyaazhar <108216444+natasyaazhar@users.noreply.github.com>
2023-12-10 16:36:51 +0800

    Update sport_shop.sql

--- sport_shop.sql
+++ sport_shop.sql
@@ -24,8 +24,9 @@ CREATE TABLE `customers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `address` text,
   `no_phone` varchar(12) DEFAULT NULL,
+  `skillset` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `hobby` text,
   `soft_delete` bigint DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -33,12 +34,12 @@ CREATE TABLE `customers` (
 
 /*Data for the table `customers` */
 
-insert  into `customers`(`id`,`name`,`email`,`address`,`no_phone`,`soft_delete`,`created_at`) values 
-(1,'jetta','jetta@gmail.com','no2, jalan austin','01234567899',1,'2023-12-10 12:39:42'),
-(2,'danish','danish@gmail.com','no29, jalan setia','0123014999',1,'2023-12-10 12:39:42'),
-(3,'natasya','natasya@gmail.com','no1','012',1,'2023-12-10 12:39:13'),
-(4,'qamarina','qamarina@gmail.com','no 2','0123456',1,'2023-12-10 12:39:13'),
-(5,'ad','ad@gmail.com','no3','019',1,'2023-12-10 12:39:13');
+insert  into `customers`(`id`,`name`,`email`,`no_phone`,`skillset`,`hobby`,`soft_delete`,`created_at`) values 
+(1,'jetta','jetta@gmail.com','01234567899','draw','reading',0,'2023-12-10 16:34:06'),
+(2,'danish','danish@gmail.com','0123014999','paint art','color',0,'2023-12-10 16:32:59'),
+(3,'natasya','natasya@gmail.com','012','planting','cooking',0,'2023-12-10 16:32:35'),
+(4,'qamarina','qamarina@gmail.com','0123456','sewing','sewing',0,'2023-12-10 16:32:23'),
+(5,'ad','ad@gmail.com','019','cooking','playing football',0,'2023-12-10 16:32:04');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
